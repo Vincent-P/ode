7d5132c6f2a73248c3915a12825679c6b4b32ede
Vincent Parizet <code@vincentparizet.com>
2024-08-13 23:22:46 +0200

    move app to samples folder

--- compile_clang.bat
+++ compile_clang.bat
@@ -5,7 +5,7 @@ for /f "tokens=1,* delims= " %%a in ("%*") do set ALL_BUT_FIRST=%%b
 :: -ffreestanding disables startup code
 :: -fuse-ld=lld forces clang to use lld instead of linker.exe
 :: -fno-stack-check, -fno-stack-protector, -mno-stack-arg-probe disables stack checks that relies on CRT
-clang.exe src/main.c -o build/%1.exe ^
+clang.exe src/cli.c -o build/ode.exe ^
 	  -O0 -g -std=c11 ^
 	  -isystem "C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\um" ^
 	  -isystem "C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\shared" ^
@@ -15,16 +15,4 @@ clang.exe src/main.c -o build/%1.exe ^
 	  -Wall -Wextra -Wpedantic -Wconversion -Wmissing-prototypes -Wimplicit-fallthrough -Wno-c2x-extensions -Wno-unused-parameter -Wno-unused-function^
 	  -DUNITY_BUILD ^
 	  %ALL_BUT_FIRST% ^
-	  -fuse-ld=lld -Xlinker /SUBSYSTEM:windows -lkernel32 -Xlinker /STACK:0x100000,0x100000 
-
-clang.exe src/cli.c -o build/%1-cli.exe ^
-	  -O0 -g -std=c11 ^
-	  -isystem "C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\um" ^
-	  -isystem "C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\shared" ^
-	  -isystem "src/stub" ^
-	  -nostdlib ^
-	  -mno-stack-arg-probe ^
-	  -Wall -Wextra -Wpedantic -Wconversion -Wmissing-prototypes -Wimplicit-fallthrough -Wno-c2x-extensions -Wno-unused-parameter -Wno-unused-function^
-	  -DUNITY_BUILD ^
-	  %ALL_BUT_FIRST% ^
-	  -fuse-ld=lld -Xlinker /SUBSYSTEM:windows -lkernel32 -Xlinker /STACK:0x100000,0x100000  -Xlinker /ltcg
+	  -fuse-ld=lld -Xlinker /SUBSYSTEM:console -lkernel32 -Xlinker /STACK:0x400,0x400  -Xlinker /ltcg
